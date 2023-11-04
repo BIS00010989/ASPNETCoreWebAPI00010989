8a45b0b7e56fa3317e3459d0f4b5ae846b3822fe
“BIS00010989” <v.korolenko303@gmail.com>
2023-11-04 12:16:00 +0500

    third commit

--- ASPNETCoreWebAPI00010989/Program.cs
+++ ASPNETCoreWebAPI00010989/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddTransient<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
